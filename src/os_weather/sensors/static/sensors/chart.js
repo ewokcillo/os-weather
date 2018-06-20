a5009ed8ba135ebdeca66150073a0b698a379cbd
Ewokcillo <azotillo@gmail.com>
2018-06-20 17:51:58 +0200

    Added load chart dataset in javascript

--- src/os_weather/sensors/static/sensors/chart.js
+++ src/os_weather/sensors/static/sensors/chart.js
@@ -1,3 +1,5 @@
+var chart = null;
+
 $("#chart-form").submit(function(event){
     event.preventDefault(); //prevent default action
     var post_url = $(this).attr("action"); //get form action url
@@ -8,80 +10,49 @@ $("#chart-form").submit(function(event){
         url : post_url,
         type: request_method,
         data : form_data
-    }).done(function(response){ //
-        //TODO
-        //update the labels accord to the granularity
-        //update the number of datasets accord to the signals
-        //update the data of each signal
-        //update the title text with the sensor name
+    }).done(function(response){
+        var labels_final = [];
+        var datasets = [];
+        $.each(response, function(index, item){
+            var labels = [];
+            var data = [];
+            $.each(item, function(item_index, tuple){
+                data.push(tuple[1]);
+                labels.push(tuple[0]);
+            });
+            var dataset = {
+		        label: index,
+		        borderColor: window.chartColors.red,
+		        backgroundColor: window.chartColors.red,
+		        fill: false,
+		        data: data,
+    		    yAxisID: index,
+        	};
+            datasets.push(dataset);
+            labels_final = labels;
+        });
+        chart = newChart(datasets, labels_final);
     });
 });
 
-var lineChartData = {
-	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-	datasets: [{
-		label: 'My First dataset',
-		borderColor: window.chartColors.red,
-		backgroundColor: window.chartColors.red,
-		fill: false,
-		data: [
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor()
-		],
-		yAxisID: 'y-axis-1',
-	}, {
-		label: 'My Second dataset',
-		borderColor: window.chartColors.blue,
-		backgroundColor: window.chartColors.blue,
-		fill: false,
-		data: [
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor()
-		],
-		yAxisID: 'y-axis-2'
-	}]
-};
 
-window.onload = function() {
-	var ctx = document.getElementById('canvas').getContext('2d');
-	window.myLine = Chart.Line(ctx, {
-		data: lineChartData,
-		options: {
-			responsive: true,
-			hoverMode: 'index',
-			stacked: false,
-			title: {
-				display: true,
-				text: ''
-			},
-			scales: {
-				yAxes: [{
-					type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-					display: true,
-					position: 'left',
-					id: 'y-axis-1',
-				}, {
-					type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-					display: true,
-					position: 'right',
-					id: 'y-axis-2',
-
-					// grid line settings
-					gridLines: {
-						drawOnChartArea: false, // only want the grid lines for one axis to show up
-					},
-				}],
-			}
-		}
-	});
+var newChart = function(datasets, labels){
+    var ctx = document.getElementById('canvas').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'line',
+	    data:  {
+            labels: labels,
+            datasets: datasets
+        },
+    	options: {
+    		responsive: true,
+    		hoverMode: 'index',
+    		stacked: false,
+    		title: {
+    			display: true,
+	    		text: 'OS-WEATHER'
+    		},
+	    }
+    });
+    return chart;
 };
