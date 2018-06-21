c6a684f5a3c0bf624af0ee1d5c987643d3c8ee57
Ewokcillo <azotillo@gmail.com>
2018-06-21 13:09:57 +0200

    Added feature to insert data in chart.js and created dataset

--- src/os_weather/sensors/static/sensors/chart.js
+++ src/os_weather/sensors/static/sensors/chart.js
@@ -1,10 +1,20 @@
 var chart = null;
 
 $("#chart-form").submit(function(event){
-    event.preventDefault(); //prevent default action
-    var post_url = $(this).attr("action"); //get form action url
-    var request_method = $(this).attr("method"); //get form GET/POST method
-    var form_data = $(this).serialize(); //Encode form elements for submission
+    event.preventDefault();
+    var post_url = $(this).attr("action");
+    var request_method = $(this).attr("method");
+    var form_data = $(this).serialize();
+
+    var chartColors = [
+	    'rgb(255, 99, 132)',
+	    'rgb(255, 159, 64)',
+	    'rgb(255, 205, 86)',
+	    'rgb(75, 192, 192)',
+	    'rgb(54, 162, 235)',
+	    'rgb(153, 102, 255)',
+	    'rgb(201, 203, 207)'
+    ];
 
     $.ajax({
         url : post_url,
@@ -13,6 +23,7 @@ $("#chart-form").submit(function(event){
     }).done(function(response){
         var labels_final = [];
         var datasets = [];
+        var scales = [];
         $.each(response, function(index, item){
             var labels = [];
             var data = [];
@@ -20,24 +31,36 @@ $("#chart-form").submit(function(event){
                 data.push(tuple[1]);
                 labels.push(tuple[0]);
             });
+
+            var colorIndex = Math.floor(Math.random()*chartColors.length);
+            var color = chartColors[colorIndex];
+            chartColors.splice(colorIndex, 1);
             var dataset = {
 		        label: index,
-		        borderColor: window.chartColors.red,
-		        backgroundColor: window.chartColors.red,
+		        borderColor: color,
+		        backgroundColor: color,
 		        fill: false,
 		        data: data,
-    		    yAxisID: index,
+		        yAxisID: index,
         	};
+        	var scale =  {
+			    type: 'linear',
+				display: true,
+				position: 'left',
+				id: index,
+			}
+
             datasets.push(dataset);
+            scales.push(scale);
             labels_final = labels;
         });
-        chart = newChart(datasets, labels_final);
+        chart = newChart(datasets, labels_final, scales);
     });
 });
 
 
-var newChart = function(datasets, labels){
-    var ctx = document.getElementById('canvas').getContext('2d');
+var newChart = function(datasets, labels, scales){
+    var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
         type: 'line',
 	    data:  {
@@ -52,6 +75,9 @@ var newChart = function(datasets, labels){
     			display: true,
 	    		text: 'OS-WEATHER'
     		},
+    		scales: {
+				yAxes: scales,
+			},
 	    }
     });
     return chart;
